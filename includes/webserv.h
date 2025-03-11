75f203e23bc25b0b118869b1666229baa8b0ee5d
Loup Thevenin <loup.thevenin.1@gmail.com>
2025-03-11 12:18:55 +0100

    add Epoll

---
+++ includes/webserv.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   webserv.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ltheveni <ltheveni@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/11 12:15:33 by ltheveni          #+#    #+#             */
+/*   Updated: 2025/03/11 12:16:58 by ltheveni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEBSERV_H
+# define WEBSERV_H
+
+# include "Epoll.hpp"
+# include "Socket.hpp"
+# include <cerrno>
+# include <cstddef>
+# include <cstdlib>
+# include <iostream>
+# include <netinet/in.h>
+# include <sys/socket.h>
+# include <vector>
+
+#endif
